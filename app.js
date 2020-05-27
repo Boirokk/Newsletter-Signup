f833ff5e565f381985597c1a9910ae5bcbcf248b
Boirokk <chadzx10@gmail.com>
2020-05-26 20:55:01 -0400

    Added express.static
    To server static image and css

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/signup.html");
+});
 
-
-
-app.listen(3000, () => console.log('Listening on 3000'))
+app.listen(3000, () => console.log("Listening on 3000"));
