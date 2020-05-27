3a524e154ba366eb7a803f80bb794ca66294e828
Boirokk <chadzx10@gmail.com>
2020-05-26 21:04:48 -0400

    Connected form to server.
    Connected and returned information from form to server.

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const request = require("request");
 
 const app = express();
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/signup.html");
 });
 
-app.listen(3000, () => console.log("Listening on 3000"));
+app.post("/", (req, res) => {
+	var fname = req.body.fname;
+	var lname = req.body.lname;
+	var email = req.body.email;
+	console.log(fname, lname, email);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log("Listening on 3000"));
